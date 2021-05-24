2a2b78ed24c9a5bb2ef0f1d40d6be0a12c8df025
Trey K <80800860+treykapfer@users.noreply.github.com>
2021-05-24 17:18:33 -0500

    grid done

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d'); /*creates 2d drawing methods*/
-console.log(ctx);
 canvas.width = 900;
 canvas.height = 600;
 
@@ -9,8 +8,17 @@ const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
 
+// mouse
+const mouse = {
+    x: undefined,
+    y: undefined,
+    width: 0.1,
+    height: 0.1
+}
 
-// game board //'
+let canvasPosition = canvas.getBoundingClientRect();
+
+// game board //
 const controlsBar = {
     width: canvas.width,
     height: cellSize,
@@ -28,18 +36,38 @@ class Cell {
         ctx.strokeRect(this.x, this.y, this.width, this.height);
     }
 }
+
+const createGrid = () => {
+    for (let y = cellSize; y < canvas.height; y += cellSize){
+        for (let x=0; x < canvas.width; x += cellSize){
+            gameGrid.push(new Cell(x, y));
+        }
+    }
+}
+
+const handleGameGrid = () => {
+    for (let i = 0; i < gameGrid.length; i++){
+        gameGrid[i].draw();
+    }
+}
 // projectiles //
 // defenders //
 // enemies //
 // resources //
 
 // utilities //
+
+//ANIMATION GAME LOOP
 const animate = () => {
     ctx.fillStyle = 'blue';
     //this is passing the game board constrolsBar
     ctx.fillRect(0,0,controlsBar.width, controlsBar.height);
+    handleGameGrid();
     requestAnimationFrame(animate);
     //this is a recursive animation loop//
 }
 
+//CALL FUNCTIONS
+createGrid();
 animate();
+console.log(gameGrid);
