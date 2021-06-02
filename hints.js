9360cf631b389f9a84ac72c072efc6a9ada05a2a
Trey K <80800860+treykapfer@users.noreply.github.com>
2021-06-02 17:46:35 -0500

    adding hint bar soon

---
+++ hints.js
@@ -0,0 +1,13 @@
+let hints = [];
+hints.push("Defenders can be upgraded twice, once for 500 and again for 1000 credits");
+hints.push("Destroyers are our most powerful upgraded defenders. Their flamethrowers will smoke anything that gets close.");
+hints.push("Failure to defend our extractors as they collect morassium will result in a paycheck deduction.");
+hints.push("The High Earth Junta will exchange your morassium for credits after each level.");
+hints.push("We've tracked enemy movements... They're getting faster after each wave!");
+hints.push("Large flying bugs approaching after level 4.");
+hints.push("The longer we take to extract these rocks, the more bugs we attract");
+hints.push("We may have to throw some grunts in front of our most valuable assets in order to survive");
+
+const cycleHints = () => {
+
+}
